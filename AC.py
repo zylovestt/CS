da28d8802b013c5688921778b8e5b951d031c372
zylovestt <2697088042@qq.com>
2022-06-19 13:18:48 +0800

    Adam->SGD

--- AC.py
+++ AC.py
@@ -196,7 +196,8 @@ class ACTWOSTEPS(ActorCritic):
 class ActorCritic_Double:
     def __init__(self,input_shape:tuple,num_subtasks,lr,weights,gamma,device,clip_grad,beta,n_steps):
         self.agent=AGENT_NET.DoubleNet(input_shape,num_subtasks).to(device)
-        self.agent_optimizer=torch.optim.Adam(self.agent.parameters(),lr=lr,eps=1e-3)
+        #self.agent_optimizer=torch.optim.Adam(self.agent.parameters(),lr=lr,eps=1e-3)
+        self.agent_optimizer=torch.optim.SGD(self.agent.parameters(),lr=lr,momentum=0.9)
         self.input_shape=input_shape
         self.num_processors=input_shape[0]
         self.num_subtasks=num_subtasks
